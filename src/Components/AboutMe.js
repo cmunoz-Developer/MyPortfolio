cb2bf4e20893722582c14052fabcfaebf336f6fc
cmunoz-Developer <cmunoz.developer@gmail.com>
2023-04-01 20:00:32 +0200

    Update card About Me

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -4,17 +4,15 @@ export const AboutMe=({name, profesion, experience, experience2, stack, master})
     return (
     <div className={styles.containerAboutMe}>
         <div className={styles.containerCard}>
-
-
+            <h1>Frontend Developer Junior</h1>
             <p className={styles.textDetails}>
-                <h1>Frontend Developer Junior</h1>
             <div className={styles.ImgDev}>
             </div>
                 Hello, I'm <span className={styles.name}>{name}</span><br/>
                 I'm a <span className={styles.textProfesion}> {profesion}</span><br/>
                 <span>{experience}</span><span>{experience2}</span><br/>
-                At the moment i'm <span className={styles.textStack}>{stack}</span><br/>
-                I have a <span className={styles.textMaster}>{master}</span>
+                <p className={styles.lineStack}>At the moment i'm <span className={styles.textStack}>{stack}</span></p>
+                <p className={styles.lineMaster}>I have a <span className={styles.textMaster}>{master}</span></p>
             </p>
         </div>
     </div>
